7badbedf1d7774337ac1119c95aac50e2997f659
ViniSouzaCosta <vinicius.souzacamargocosta@gmail.com>
2025-11-10 22:15:25 -0300

    att

--- atv_front/src/pages/index.js
+++ atv_front/src/pages/index.js
@@ -1,45 +1,26 @@
-import Head from 'next/head';
-import { useEffect, useState } from 'react';
-import MovieList from '../components/MovieList';
+import Head from "next/head";
+import Container from "@/components/Container";
+import LoginContent from "@/components/LoginContent";
+import Footer from "@/components/Footer";
 
 export default function Home() {
-  const [movies, setMovies] = useState([]);
-  const [error, setError] = useState(null);
-
-
-  const API_URL = 'http://localhost:3001/movies';
-
-  useEffect(() => {
-    async function fetchMovies() {
-      try {
-        const res = await fetch(API_URL);
-        if (!res.ok) throw new Error(`Erro HTTP: ${res.status}`);
-        const data = await res.json();
-        setMovies(data);
-      } catch (err) {
-        setError(err.message);
-      }
-    }
-    fetchMovies();
-  }, []);
-
   return (
     <>
       <Head>
-        <title>Lista de Filmes</title>
+        <title>Movies &copy; 2025</title>
+        <meta
+          name="description"
+          content="Consumo em React de uma API de Filmes"
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
       </Head>
-
-      <main style={{ maxWidth: 800, margin: '24px auto', padding: '0 16px' }}>
-        <h1>🎬 Catálogo de Filmes</h1>
-
-        {error ? (
-          <p style={{ color: 'red' }}>Erro ao carregar filmes: {error}</p>
-        ) : movies.length > 0 ? (
-          <MovieList movies={movies} />
-        ) : (
-          <p>Carregando filmes...</p>
-        )}
+      <main>
+        <Container>
+          <LoginContent />
+        </Container>
       </main>
+      <Footer />
     </>
   );
 }
